9a198ae589f9a4d360b0738d5ed5970a355f3e37
JacobWesthoff04 <mzufnz@gmail.com>
2025-03-25 01:28:26 -0500

    Added files

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create a Cat object
+        Cat simon = new Cat("Simon", 9, "Tabby", 11.0);
+
+        // Display initial info
+        simon.displayInfo();
+
+        // TODO: Modify this file to use reflection for:
+        // 1. Listing all fields.
+        // 2. Listing all methods.
+        // 3. Changing private field access and modifying ALL values.
+        // 4. Changing private method access and invoking ALL of them.
+    }
+}
